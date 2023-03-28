b6fc6c61e6ec11ac42047810b27d5d5c70a6e02d
Sina Kian <70090343+kiansina@users.noreply.github.com>
2023-03-28 17:09:41 +0200

    Update tset.py

--- tset.py
+++ tset.py
@@ -104,6 +104,8 @@ if check_password():
     #else:
     #    st.session_state["aut"] = 'sup'
 
-    #st.write(st.session_state["username"].lower())
-    df['id', 'domiciliatario', {}.format(st.session_state["username"].lower()), 'rating_base']
+    
+          
+    xx=['id', 'domiciliatario', 'rating_base']+[st.session_state["username"].lower()]
+    st.write(xx)
     e_df = st.experimental_data_editor(df, num_rows="dynamic")
